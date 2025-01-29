5968e50d3d8a1e30ba88c95d2f381457a123dc65
Benjamin Kahkeshan <benkah@kth.se>
2025-01-29 19:10:52 +0100

    Router
    change routing

--- recruitment-app/src/App.js
+++ recruitment-app/src/App.js
@@ -1,15 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
-import React from 'react';
-import Auth from './views/Auth';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ApplicantProfilePresenter from "./presenters/ApplicantProfilePresenter";
+import ApplicantProfileView from "./views/ApplicantProfileView";
+import RecruitmentModel from "./models/RecruitmentModel";
+import Auth from "./views/Auth";
 
 function App() {
-  return (
-    <div>
-      <h1>Recruitment Portal</h1>
-      <Auth />
-    </div>
-  );
+    return (
+        <Router>
+            <div>
+                <h1>Recruitment Portal</h1>
+                {/* Define Routes */}
+                <Routes>
+                    <Route path="/" element={<Auth />} />
+                    <Route 
+                        path="/profile" 
+                        element={<ApplicantProfilePresenter model={RecruitmentModel} view={ApplicantProfileView} />} 
+                    />
+                </Routes>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
