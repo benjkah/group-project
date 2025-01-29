8402ea68d97295d112cd89668af6471b1786cca2
Benjamin Kahkeshan <benkah@kth.se>
2025-01-29 19:09:05 +0100

    test2
    test2

--- recruitment-app/src/presenters/ApplicantProfilePresenter.js
+++ recruitment-app/src/presenters/ApplicantProfilePresenter.js
@@ -2,7 +2,7 @@ import { observer } from "mobx-react-lite";
 
 export default observer(
     
-    function ApplicantProfilePresenter(props) 
+    function  ApplicantProfilePresenter(props) 
 {
 
     const { model } = props;
