612435f62da5471339134fe8ab773baddc7943da
Magnus <mtry@kth.se>
2025-02-04 12:18:40 +0100

    DB setup

--- recruitment-app/src/index.js
+++ recruitment-app/src/index.js
@@ -1,3 +1,5 @@
+
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
@@ -15,3 +17,28 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
+var Connection = require('tedious').Connection;  
+    var config = {  
+        server: '1201group8server.database.windows.nett',
+        authentication: {
+            type: 'default',
+            options: {
+                userName: 'rootadmin', 
+                password: 'qwerty123!'
+            }
+        },
+        options: {
+            // If you are on Microsoft Azure, you need encryption:
+            encrypt: true,
+            database: 'IV1201Group8' 
+        }
+    };  
+    var connection = new Connection(config);  
+    connection.on('connect', function(err) {  
+        // If no error, then good to proceed.
+        console.log("Connected");  
+    });
+    
+    connection.connect();
