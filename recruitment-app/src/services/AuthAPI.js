f1edd8f9a0624120d5d792d6c900a4324f86b3db
Benjamin Kahkeshan <benkah@kth.se>
2025-02-07 20:14:18 +0100

    Update AuthAPI.js
    login fixed

--- recruitment-app/src/services/AuthAPI.js
+++ recruitment-app/src/services/AuthAPI.js
@@ -1,8 +1,7 @@
 const API_BASE_URL = "http://localhost:4000";  
-
 export async function login(username, password) {
     try {
-        const response = await fetch(`${API_BASE_URL}/login`, {  // Fix the API URL
+        const response = await fetch(`${API_BASE_URL}/login`, {  
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ username, password }),
