f286ad8dd3fb4c806179864deeb0b539d65b26df
Magnus <mtry@kth.se>
2025-02-13 21:40:52 +0100

    get recvest

--- backend/routes/create.js
+++ backend/routes/create.js
@@ -1,8 +1,41 @@
 const express = require('express');
 const router = express.Router();
+const { executeQuery } = require("../database.js");
 
-router.get("/user", (req, res) => {
-    res.send("Create user");
-})
+router.post("/user", async (req, res) => {
+    console.log(req.body);
+    const values = [req.body.name, req.body.surname, req.body.pnr, req.body.email, req.body.pasword, 1, req.body.usernamn];
+
+    const paramNames = ["name", "surname", "pnr", "email", "pasword", "role_id" ,"usernamn"];
+
+    try {
+        const query = `
+            INSERT INTO [dbo].[person]
+            ([name]
+            ,[surname]
+            ,[pnr]
+            ,[email]
+            ,[password]
+            ,[role_id]
+            ,[username])
+        VALUES (
+            name,
+            surname,
+            pnr,
+            email,
+            password,
+            role_id,
+            username)
+        `;
+        const result = await executeQuery(query, values, paramNames, false);
+
+        console.log(result);
+  
+        res.status(200).json(result);
+    } catch (error) {
+        console.error("Error fetching profile:", error);
+        res.status(500).json({ message: "Internal server error." });
+    }
+  });
 
 module.exports = router
