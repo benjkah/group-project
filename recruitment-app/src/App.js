f0923f84ed99adaa47bea412859a3bbd10b99841
benjamin <benkah@kth.se>
2025-02-19 15:36:46 +0100

    fix app.js

--- recruitment-app/src/App.js
+++ recruitment-app/src/App.js
@@ -7,54 +7,11 @@ import LoginView from './views/LoginView';
 import RegistrationView from './views/RegistrationView';
 import ApplicationListPresenter from "./presenters/ApplicationListPresenter";
 import ApplicationListView from "./views/ApplicationListView";
-import applicationModel from "./models/ApplicationModel";
+import applicationModel from "./models/ApplicationListModel";
 import axios from "axios"
 
 function App({ userModel, recruitmentModel }) {
-/*
-    const makeAPICall = async () => {
-        
-        const id = {
-            id: 2,
-            namn: "kalle"
-        }
 
-        try {
-            const response = await fetch('http://localhost:4000/user', id);
-            const data = await response.json();
-            console.log({data});
-        } catch (e) {
-            console.log(e);
-        }
-    };
-
-*/
-
-    /*const axiosPostData = async() => {
-        const postData = {
-            id: 2,
-            namn: "21"
-        }
-
-        await axios.get('http://localhost:4000/users', postData)
-        .then(res => console.log(res.data))
-    }
-
-    const [data, setData] = useState('');
-
-    useEffect(() => {
-        (async function () {
-        const { text } = await( await fetch(`/api/message`)).json();
-        setData(text);
-        })();
-        axiosPostData();
-    });
-
-    console.log(data);
-
-  console.log("App.js");
-
-  */
   return (
     <Router>
         <div>
