25b446b216c587ec2c4822434b0f684297bec232
benjamin <benkah@kth.se>
2025-03-02 02:51:30 +0100

    BASE URL

--- recruitment-app/src/services/AuthAPI.js
+++ recruitment-app/src/services/AuthAPI.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:4000';
+const API_BASE_URL = process.env.REACT_APP_BACKEND_URL || "http://localhost:4000";
+
 
 export async function login(username, password) {
   try {
