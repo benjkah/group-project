b617f405bd3a08758b92ef2b6c036e03cc3a4808
benjamin <benkah@kth.se>
2025-03-05 03:44:51 +0100

    close browser and remove coockies

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -84,7 +84,7 @@ class   Authorization {
             secure: true,  
             sameSite: "None", 
             partitioned: true,
-            expires: new Date(Date.now() + 2 * 60 * 60 * 1000)  //two ours
+            maxAge : null 
 
         });
     }
