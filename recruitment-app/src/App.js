1d23bed0e2f930b2999046f3784effbac75881a4
Rasmus Norberg Thörn <rasmus.norberg1@gmail.com>
2025-03-09 19:49:13 +0100

    userModel.isLoggedIn in dependency array

--- recruitment-app/src/App.js
+++ recruitment-app/src/App.js
@@ -57,7 +57,7 @@ function App({ recruitmentModel, applicationListModel, userModel }) {
         );
 
         return () => updateUI(); // Cleanup
-    }, []);
+    }, [userModel.isLoggedIn]);
 
     if (!isAuthChecked) return <div>Loading...</div>;
 
