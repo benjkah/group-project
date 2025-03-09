d3154d5b4db750ed48c2302646dbf921cdb6bdfe
benjamin <benkah@kth.se>
2025-03-09 10:15:06 +0100

    add partition2

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -83,6 +83,7 @@ class Authorization {
             httpOnly: true,
             secure: true,
             sameSite: "None",
+            partitioned: true ,
             path: "/",
         });
     }
