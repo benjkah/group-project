deee5e84ad0af6ed515cb965b2b4c59e8b0231e2
benjamin <benkah@kth.se>
2025-03-10 11:33:34 +0100

    add comment for route

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -4,6 +4,19 @@ const Authorization = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
+/**
+ * Defines API routes to handle user profiles, competencies, and applications.
+ *
+ * - GET /profile: Fetches the user's profile 
+ * - DELETE /deleteAvail/:id: Removes an availability 
+ * - DELETE /deleteComp/:id: Removes a competence .
+ * - POST /addComp: Add  a new competence.
+ * - POST /addAvail: Add  a new availability.
+ * - GET /applicantProfile/:id: Fetches an applicant's profile .
+ * - POST /applicationStatus/:id: Updates the application status .
+ */
+
+
 router.get("/profile", Authorization.checkLogin, Controller.getProfile);
 router.delete("/deleteAvail/:id", Controller.deleteAvailability);
 router.delete("/deleteComp/:id", Controller.deleteCompetence);
